4958ab0a49843f3e7e44f31c9668ed792e19124f
J Shiva Shankar <jangamshiva2005@gmail.com>
2024-08-11 20:43:17 +0530

    added codes

--- src/components/LetterButtons/LetterButtons.jsx
+++ src/components/LetterButtons/LetterButtons.jsx
@@ -1,13 +1,16 @@
+import { split } from "postcss/lib/list"
 
 const Alphabets = new Array(26).fill('').map((element,index)=>String.fromCharCode(65+index))
 
-function LetterButtons({usedLetters,onLetterClick}){
+function LetterButtons({text,usedLetters,onLetterClick}){
+
+    const originalCharacters = new Set(text.toUpperCase(),split(''));
 
     const selectedLetters = new Set(usedLetters.join('').toUpperCase().split(''))
 
     const buttonStyle = function(letter){
         if(selectedLetters.has(letter)){
-            return ' bg-red-500 border-red-700 hover:bg-red-700'
+            return  `bg-red-500 ${originalCharacters.has(letter) ? 'bg-green-500 border-green-700 hover:bg-green-700'  : 'border-[#000] border-4 hover-bg-red-700' } `
         }else{
             return ' bg-blue-500 border-blue-700 hover:bg-blue-700 ' 
 
--- src/pages/PlayGamepage/PlayGame.jsx
+++ src/pages/PlayGamepage/PlayGame.jsx
@@ -1,19 +1,27 @@
 import { Link } from "react-router-dom";
 import MaskedText from "../../components/MaskedText/MaskedText";
 import LetterButtons from "../../components/LetterButtons/LetterButtons";
+import { useState } from "react";
 
 // PlayGame.js
 function PlayGame() {
+
+    const [usedLetters, setusedLetters] = useState([])
+
+    const HandleLetterClick = function(letter){
+        setusedLetters([...usedLetters,letter])
+    }
+
     return (
         <div>
             <h1>Play Game</h1>
-            <MaskedText text="humble" usedLetters={['b','e']}/>
+            <MaskedText text="humble" usedLetters={usedLetters}/>
             <hr />
 
-            <LetterButtons usedLetters={['b','e']}/>
+            <LetterButtons text='humble' usedLetters={usedLetters} onLetterClick={HandleLetterClick}/>
             <hr />
             
-            <Link to='/start'>Start Game</Link>
+            <Link to='/start' className="text-blue-600 underline hover:text-blue-700">Start Game</Link>
         </div>
     );
 }
